5ba160bc59d80b301034e735982d3a72adf78564
Yinel Ayala <>
2021-04-27 04:26:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let nombre_est= prompt ('indique su nombre');
+console.log('el nombre del estudiante es',nombre_est)
+let edad_est= prompt ('indique la edad')
+console.log('la edad del estudiante es',edad_est)
